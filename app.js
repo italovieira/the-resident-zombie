482c5b6fd7182d41eaf9cf3bdbd54c273558c06a
Ítalo Vieira <italogomesv@gmail.com>
2020-07-15 08:49:50 -0300

    Create basic app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const logger = require('morgan')
+
+const indexRouter = require('./routes/index')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', indexRouter)
+
+app.listen(3000)
+
+module.exports = app
