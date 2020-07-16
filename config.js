302072dd1d174449584b31e94a131d9d654d338b
Ítalo Vieira <italogomesv@gmail.com>
2020-07-16 19:30:49 -0300

    Pass connection options in mongo URI

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ module.exports = {
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_NAME: process.env.DB_NAME,
+  DB_OPTIONS: process.env.DB_OPTIONS,
 }
--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = require('./config')
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DB_OPTIONS,
+} = require('./config')
 
-const mongoUri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+const mongoUri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?${DB_OPTIONS}`
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 module.exports = mongoose
