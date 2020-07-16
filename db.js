71776db145ffe826c074b6614e2f804e0979e3ea
Ítalo Vieira <italogomesv@gmail.com>
2020-07-16 19:22:10 -0300

    Solve DeprecationWarning in mongo connection

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = require('./config')
 
 const mongoUri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
-mongoose.connect(mongoUri, { useNewUrlParser: true })
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 module.exports = mongoose
