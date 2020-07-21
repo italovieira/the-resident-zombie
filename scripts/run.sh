69c884c57fee95c52091c8f0d03abf7812c63c74
Ítalo Vieira <italogv@hotmail.com>
2020-07-21 18:04:50 -0300

    Make run and stop script work regardless of the path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-cd ..
+
+# docker-compose.yml path
+APP_ROOT_PATH="$(dirname "${BASH_SOURCE[0]}")"/..
+cd $APP_ROOT_PATH
+
 docker-compose run --rm api npm test
 docker-compose up -d
 docker-compose exec api node bin/seedDatabase
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-cd ..
+
+# docker-compose.yml path
+APP_ROOT_PATH="$(dirname "${BASH_SOURCE[0]}")"/..
+cd $APP_ROOT_PATH
+
 docker-compose down
